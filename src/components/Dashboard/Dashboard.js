bc3cc5c06e8772d51d176a449d079a0933fdc81f
MagicManik <manikislam1952@gmail.com>
2022-04-04 00:27:29 +0600

    all component create done but still now load data and design part has remained

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>This is my Dashboard Compoooo</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
