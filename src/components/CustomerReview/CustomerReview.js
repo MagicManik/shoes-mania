1066bdbf0a6e39c1aa64e4ec50e7dd0e7b79eccd
MagicManik <manikislam1952@gmail.com>
2022-04-04 08:28:32 +0600

    home component complited

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,12 +1,22 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './CustomerReview.css'
 
 const CustomerReview = ({ customer }) => {
+    console.log(customer.name)
     const { img, name, review } = customer;
     return (
         <div className='customer-review'>
             <img src={img} alt="" />
-            <p>{name}</p>
+            <div className='customer-review-text'>
+                <h5>{name}</h5>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+            </div>
             <p>{review}</p>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import shoe from '../../showcase-area-img/shoe.png'
 import useCustomers from '../../hooks/useCustomers';
 import './Home.css'
 import CustomerReview from '../CustomerReview/CustomerReview';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
     const [customers, setCutomers] = useCustomers();
     // console.log(customers)
 
+    const sliceCustomer = customers.slice(1, 4);
+
+    const navigate = useNavigate();
+
+    const seeAllReviews = () => {
+        navigate('/reviews');
+    }
+
+
     return (
         <div>
-            <div className='showcase-container'>
-                <div className='showcase-text'>
-                    <h1>Your next shoes</h1>
-                    <h1><span className='best-show'>Your best shoes</span></h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+            <section className='showcase-area-bg-color'>
+                <div className='showcase-container'>
+                    <div className='showcase-text'>
+                        <h1>Your next shoes</h1>
+                        <h1><span className='best-show'>Your best shoes</span></h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                    </div>
+                    <div className='showcase-img'>
+                        <img src={shoe} alt="" />
+                    </div>
                 </div>
-                <div className='showcase-img'>
-                    <img src={shoe} alt="" />
-                </div>
-            </div>
+            </section>
 
             <section className='customer-review-section'>
                 <h1>Customer Reviews</h1>
                 <div className='customer-reviews-container'>
 
                     {
-                        customers.map(customer => <CustomerReview key={customer.id} customer={customer}></CustomerReview>)
+                        sliceCustomer.map(customer => <CustomerReview key={customer.id} customer={customer}></CustomerReview>)
                     }
 
                 </div>
+                <div className='reviews-button'>
+                    <button onClick={seeAllReviews}>See All Reviews <FontAwesomeIcon icon={faArrowRightLong}></FontAwesomeIcon></button>
+                </div>
             </section>
+
+
         </div>
     );
 };
